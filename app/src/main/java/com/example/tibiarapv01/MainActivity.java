f7feebbf3e04afd0bc5ae2781d65ff445b8c4c1d
Miguel Angel Perez <miguelpz.994@gmail.com>
2019-11-13 07:20:55 -0600

    Test From work

--- app/src/main/java/com/example/tibiarapv01/MainActivity.java
+++ app/src/main/java/com/example/tibiarapv01/MainActivity.java
@@ -79,9 +79,6 @@ public class MainActivity extends AppCompatActivity {
                            }
                            else{
 
-
-
-
                            }
                        }
 
